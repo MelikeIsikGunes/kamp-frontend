926ef84f4b4ff05e0b89e6e4817e07dce60d5d23
Melike Işık <melike.34.37@hotmail.com>
2021-03-10 23:46:23 +0300

    10 Mart Commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //using gibi
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', // ./aynı klasör demek
+  styleUrls: ['./app.component.css'] //dizi
 })
 export class AppComponent {
-  title = 'northwind';
+  title = 'northwind'; // title:string = "northwind"; diye de yazılabilirdi
+  user: string = "Engin Demiroğ"
+  
 }
+
+//html'sinin datasını yönettiğimiz yer bu component
