7b729ad09d8da20d01ec92406bb1bc14fbc9c27e
Melike Işık <melike.34.37@hotmail.com>
2021-03-23 01:32:28 +0300

    20 Mart Ders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"", pathMatch:"full" , component:ProductComponent}, //anasayfa adresinde hiçbir şey yazmıyorsa ProductComponent aç 
   {path:"products", component:ProductComponent}, // localhost:4200/products yazılsa da onu aç
-  {path:"products/category/:categoryId", component:ProductComponent}
-//parametre için : kullanıyoruz
+  {path:"products/category/:categoryId", component:ProductComponent},
+  {path:"products/add", component:ProductAddComponent}
+  //parametre için : kullanıyoruz
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'; //biz ekledik. Db bağlanmak için
-import {FormsModule} from "@angular/forms"  //biz ekledik. ngModel çalışsın diye
+import {FormsModule, ReactiveFormsModule} from "@angular/forms"  //biz ekledik. ngModel çalışsın diye
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations" //biz ekledik. Animasyonlar kullanmadan, web sayfası geçişleri ani ve rahatsız edici görünebilir.
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +13,8 @@ import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import {ToastrModule} from "ngx-toastr";
-import { CartSummaryComponent } from './components/cart-summary/cart-summary.component'
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component'
 
 @NgModule({
   declarations: [ //kullanılacak componentler buraya eklenir
@@ -23,7 +24,8 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     NaviComponent,
     VatAddedPipe,
     FilterPipePipe,
-    CartSummaryComponent
+    CartSummaryComponent,
+    ProductAddComponent
   ],
   imports: [  //dışarıdan bizim yazmadığımız modüller buraya eklenir. (Http,Toastr,Forms.. biz ekledik)
     BrowserModule,
@@ -31,6 +33,7 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right" //ekranda gözükmesi istenen yer, alt sağ
     })
